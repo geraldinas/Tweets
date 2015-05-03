163bf457509fe149c95a29adc02a1bf66e386959
geraldinas <geraldinagalvarez@gmail.com>
2015-05-03 18:43:32 -0400

    tweet haiku shows up in views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
 
   def self.get_obama_tweets
-    @tweetarray ||= []
-    $client.user_timeline("barackobama").each do |tweet|
+    @tweetarray = []
+    $client.user_timeline("kanyewest", count: 500).each do |tweet|
       @tweetarray << tweet
     end
     @tweetarray
@@ -12,7 +12,7 @@ class Post < ActiveRecord::Base
     regex_words = []
     @tweetarray.each do |word_array|
       @tweetdupe = word_array.text.dup
-      regex_words << @tweetdupe.gsub(/#(.*)/, "").gsub(/http(.*)/, "").gsub(/@(.*)/, "").gsub(/RT/,"").gsub(".","")
+      regex_words << @tweetdupe.gsub(/#(.*)|http(.*)|@(.*)|RT|\.|\W|\d/, "")#.gsub(/http(.*)/, "").gsub(/@(.*)/, "").gsub(/RT/,"").gsub(".","")
     end
     regex_words
   end
@@ -27,12 +27,54 @@ class Post < ActiveRecord::Base
 
 
   def self.word_randomizer
-    @randomized = split_word.sample(140)
+    @randomized = split_word.shuffle
   end
 
   def self.syllable_count
     word_randomizer.each_with_object(Hash.new('')) do |word, combos|
         combos[word] = word.count_syllables
     end
+  end
+
+  def self.five_syllable_combo_maker
+    sum = 0
+    words = []
+
+    syllable_count.each do |word, syl_value|
+      if (sum + syl_value) <= 5
+        words << word 
+        sum += syl_value
+      end 
+    end
+    words
+  end
+
+  def self.seven_syllable_combo_maker
+    sum = 0
+    words = []
+
+    syllable_count.each do |word, syl_value|
+      if (sum + syl_value) <= 7
+        words << word 
+        sum += syl_value
+      end 
+    end
+    words
+  end
+
+  def self.get_list_five_syllables(n = 10)
+    syllables = []
+    n.times do 
+      syllables << five_syllable_combo_maker
+    end
+    syllables
+  end
+
+  def self.get_list_seven_syllables(n = 10)
+    syllables = []
+    n.times do 
+      syllables << seven_syllable_combo_maker
+    end
+    syllables
   end
 end
