86fceca1027fdfa4aa5c835e4b0dabfe2ddbd877
mandelbulb <mandelbulb@github.com>
2015-05-04 17:36:21 -0400

    afasdfasd

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   
   def create
     @post = Post.new(post_params)
-      redirect_to "/posts/#{@post.id}"
+    redirect_to "/posts/#{@post.id}"
   end
 
   def new
@@ -12,8 +12,6 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @complete_haiku = @post.complete_haiku
-    #@five_sentence = @post.get_list_five_syllables(2)
-    #@seven_sentence = @post.get_list_seven_syllables(1)
   end
 
   private
